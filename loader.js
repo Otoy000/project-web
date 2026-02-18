ac4eb4d87209e3fdc76acbd83cf40cce4e8d8c6b
ammarkhocan <ammarkhocan02@gmail.com>
2026-02-18 17:28:27 +0700

    fix loader home

--- loader.js
+++ loader.js
@@ -1,4 +1,12 @@
-window.addEventListener('load', () => {
-  const loader = document.getElementById('loader');
-  loader.classList.add('hide');
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader.classList.add("hide");
+    document.body.classList.remove("loading");
+  }, 300);
+
+  loader.addEventListener("transitionend", () => {
+    loader.remove();
+  });
 });
